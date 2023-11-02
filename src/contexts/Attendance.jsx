9bb01dd3a1dd742fcf361170aa6874e6eba30d42
saikat0511 <saikat0511@gmail.com>
2023-11-02 18:58:32 +0530

    refresh cookie automatically

--- src/contexts/Attendance.jsx
+++ src/contexts/Attendance.jsx
@@ -1,6 +1,8 @@
 import React, { createContext, useEffect, useState } from 'react';
 import useAuthContext from '../hooks/useAuthContext';
 import { ATTENDANCE } from '../constants/endpoints';
+import storage from '../helpers/mmkv';
+import useLogin from '../hooks/useLogin';
 
 export const AttendanceContext = createContext();
 
@@ -18,6 +20,23 @@ export function AttendanceContextProvider({ children }) {
   const [isLoading, setIsLoading] = useState(false);
   const [attendance, setAttendance] = useState([]);
   const { user } = useAuthContext();
+  const { login } = useLogin();
+
+  const getAttendance = async () => {
+    const response = await fetch(ATTENDANCE, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        cookie: user.cookie,
+        year: yearSession.year,
+        session: yearSession.session,
+      }),
+    });
+
+    const json = await response.json();
+
+    return [response, json];
+  };
 
   useEffect(() => {
     if (yearSession) {
@@ -25,20 +44,16 @@ export function AttendanceContextProvider({ children }) {
         setIsLoading(true);
         setError(null);
 
-        const response = await fetch(ATTENDANCE, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            cookie: user.cookie,
-            year: yearSession.year,
-            session: yearSession.session,
-          }),
-        });
-
-        const json = await response.json();
+        const [response, json] = await getAttendance();
 
         if (!response.ok) {
           setError(json.error);
+          if (json.error === 'invalid cookie') {
+            console.log(json.error);
+            const username = storage.getNumber('username');
+            const password = storage.getString('password');
+            await login(username, password);
+          }
         } else {
           const tempAttendance = [];
           for (let i = 0; i < json.subject.length; i += 1) {
